248f76e2916c48204bbf9b0c3b47fba89d6b3204
Aleksandar Zaharinov <161043636+AlexanderZ10@users.noreply.github.com>
2024-03-14 15:39:16 +0200

    added events function

--- app.py
+++ app.py
@@ -49,7 +49,38 @@ def login():
         if data["email"] == user["email"] and data["password"] == user["password"]:
             return jsonify(user)       
     return jsonify({'error': 'user not found'}), 404
+events = []
 
 
+@app.route("/events")
+def list_events():
+    return jsonify(events)
 
+@app.route("/events/create", methods=['POST'])
+def create_events():
+    data = request.json
+    if 'name_event' not in data or 'description' not in data:
+        return jsonify({'error': 'name and description are required'}), 400
+    
+    name_event = data["name_event"]
+    description = data["description"]
+    photo = data["photo"]
 
+    # Check if the username is already taken
+    for event in events:
+        if event["name_event"] == name_event:
+            return jsonify({'error': 'event already exists'}), 400
+    for event in events:
+        if event["description"] == description:
+            return jsonify({'error': 'event already exists'}), 400
+
+    # Store the user information in the database
+    event = {
+        "name_event" : name_event,
+        "description" : description,
+        "photo" : photo
+    }  
+
+    events.append(event)
+
+    return jsonify({'message': 'Event created successfully'})
