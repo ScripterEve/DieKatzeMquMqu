354ca13ed1d9cd9b0ba5fdb83b6e9b32ef67e4cd
Eva Georgieva <158205304+ScripterEve@users.noreply.github.com>
2024-03-15 10:36:43 +0200

    Added colours to the js pallete and the 2nd Event of the section with all of the events

--- javascript/pallete.js
+++ javascript/pallete.js
@@ -7,6 +7,14 @@ tailwind.config = {
                 'gg-dark-gray': '#BBC6C8',
                 'gg-light-gray': '#E5E3E4',
                 'gg-accent': '#DDBEAA',
+                
+
+
+                'gg-cream': '#f8f1e5',
+                'gg-brown': '#7e685a',
+                'gg-pale-pink': '#c8a6a6',
+                'gg-pale-yellow': '#e6dbc2',
+                'gg-pale-teal': '#9fb8ad',
             }
         }
     }
