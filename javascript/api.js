0d5ce485ace3e9dffb56f7db9d82151f42788452
mariyancholakov <m.cholakov09@gmail.com>
2024-03-15 22:53:02 +0200

    Changed design of home page

--- javascript/api.js
+++ javascript/api.js
@@ -1,5 +1,9 @@
 function login(){
 
+   const data={
+        password: document.querySelector('#password').value,
+        email: document.querySelector('#email').value
+    }
 
 fetch("/login",{
     method:"POST",
@@ -21,7 +25,38 @@ fetch("/login",{
                 document.body.appendChild(div)
             })
         } else {
+            response.json().then(answer => {
+        setCookie('name',answer.name,1)
+
+        setCookie('email',answer.email,1)
+
+        document.location.href="/"
 
         }
-    })
+            )
+    }})
 }
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+
+  function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
