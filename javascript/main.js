038d599129d572accabccb52bbe39dea07c3a001
mariyancholakov <m.cholakov09@gmail.com>
2024-03-15 12:42:15 +0200

    Added function "drop down function" to the nav bar

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,7 @@
-tailwind.config = {
-    theme: {
-        extend: {
-            colors: {
-                'gg-primary': '#469597',
-                'gg-secondary': '#5BA199',
-                'gg-dark-gray': '#BBC6C8',
-                'gg-light-gray': '#E5E3E4',
-                'gg-accent': '#DDBEAA',
-            }
-        }
-    }
+const dropDownMenu = document.querySelector('.events-dropdown-menu');
+const toggleLinkEvents = document.querySelector('.events-link');
+
+
+toggleLinkEvents.onclick = function(){
+    dropDownMenu.classList.toggle('open');
 }
